a0011d1a83dcc934143cf44ae4ea5d7526213bcf
Tingyu Shi <tingyushi@Tingyus-MacBook-Pro.local>
2023-06-03 22:05:40 +0800

    add bst code

---
+++ Tree/bst.py
@@ -0,0 +1,104 @@
+'''
+Binary search tree
+This binary search tree does not allow duplicated values
+'''
+
+class BSTNode:
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+
+    def is_leaf(self):
+        return self.left == None and self.right == None
+
+    def is_left_child(self):
+        return self == self.parent.left
+
+    def is_right_child(self):
+        return not self.is_left_child()
+
+    def __str__(self):
+        return "(" + str(self.value) + ")"
+
+    def __repr__(self):
+         return "(" + str(self.value) + ")"
+
+
+class BST:
+
+    def __init__(self):
+        self.root = None
+
+    def is_empty(self):
+        return self.root == None
+
+    def get_height(self):
+        if self.is_empty():
+            return 0
+        return self.__get_height(self.root)
+
+    def __get_height(self, node):
+        if node == None:
+            return 0
+        return 1 + max(self.__get_height(node.left), self.__get_height(node.right))
+
+    def insert(self, value):
+        if self.is_empty():
+            self.root = BSTNode(value)
+        else:
+            self.__insert(self.root, value)
+
+    def __insert(self, node, value):
+
+        # handle the case that the value has already existed
+        if node.value == value:
+            return
+        if value < node.value:
+            if node.left == None:
+                node.left = BSTNode(value)
+                node.left.parent = node
+            else:
+                self.__insert(node.left, value)
+        else:
+            if node.right == None:
+                node.right = BSTNode(value)
+                node.right.parent = node
+            else:
+                self.__insert(node.right, value)
+
+    # if the value is found, return the node
+    def search(self, targetValue):
+        # handle the empty tree case
+        if self.is_empty():
+            return None
+        
+        if (self.root.value == targetValue):
+            return self.root
+        else:
+            return self.__search(self.root, targetValue)
+
+    def __search(self, node, targetValue):
+
+        if node.value == targetValue:
+            return node
+        
+        '''
+        If the 
+        '''
+
+    def __str__(self):
+        if self.is_empty():
+            return "[]"
+        return "[" + self.__str_helper(self.root) + "]"
+
+    def __str_helper(self, node):
+        if node.is_leaf():
+            return "[" + str(node) + "]"
+        if node.left == None:
+            return "[" + str(node) + " -> " + self.__str_helper(node.right) + "]"
+        if node.right == None:
+            return "[" +  self.__str_helper(node.left) + " <- " + str(node) + "]"
+        return "[" + self.__str_helper(node.left) + " <- " + str(node) + " -> " + self.__str_helper(node.right) + "]"
---
+++ Tree/bstTest.py
@@ -0,0 +1,9 @@
+from bst import BST
+
+btree = BST()
+btree.insert(5)
+btree.insert(6)
+btree.insert(4)
+btree.insert(4)
+btree.insert(5)
+print(btree)
